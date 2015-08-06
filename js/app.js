efc5c793728eeb4baadd56576b8405b4e7a7afe8
unknown <ASIDDAVA@USHBKGLC5CHZW1.na.ad.guycarp.com>
2015-08-06 11:37:00 -0400

    Added routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider) {
+
+	var path = '/templates';
+	$routeProvider.when('/',{
+			templateUrl: path+'/home.html'
+		})
+		.when('/Home',{
+			templateUrl: path+'/home.html'
+		})
+		.when('/Todo',{
+			templateUrl: path+'/todo.html'
+		})
+		.otherwise({
+        	redirectTo: '/'
+      	}); 
+}]);
